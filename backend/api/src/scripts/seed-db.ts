0a6dd53b205fe21071230153331f0fe106baee72
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2021-11-04 11:09:43 -0300

    fix: nvm version, seed scripts

--- backend/api/src/scripts/seed-db.ts
+++
@@ -1,8 +0,0 @@
-import userRepository from "../repositories/userRepository";
-import databaseService from "../services/database/databaseService";
-
-async function main() {
-    await databaseService.init();
-    userRepository.create();
-}
-main();
---
+++ backend/api/src/scripts/seed-db/seed-db.ts
@@ -0,0 +1,9 @@
+// import userRepository from "../../repositories/userRepository";
+// import databaseService from "../../services/database/databaseService";
+// import users from "./users.json";
+
+// async function main() {
+//     await databaseService.init();
+//     await Promise.all(users.map(userRepository.create));
+// }
+// main();
